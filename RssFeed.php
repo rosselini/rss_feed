754925bf78f9d6034f2fd26f7673d64852ff5b33
rosselini <tortoise1206@gmail.com>
2012-08-21 14:50:23 +0200

    Change file - delete comment

--- RssFeed.php
+++ RssFeed.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of RssFeed
- *
- * @author Ariel
- */
 class RssFeed {
     private $stringRssFeed = null;
     private $rssFeedUrl = null;
